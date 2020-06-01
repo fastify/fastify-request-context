9ca404b12763e18f6a2ce750e05b22986f12b8ee
Igor Savin <iselwin@gmail.com>
2020-06-02 00:02:55 +0200

    Migrate from TS to JS to adhere to fastify org conventions (#1)

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { fastifyRequestContextPlugin } from './lib/requestContextPlugin'
-
-export type { RequestContext, RequestContextOptions } from './lib/requestContextPlugin'
-export { fastifyRequestContextPlugin }
-export default fastifyRequestContextPlugin
