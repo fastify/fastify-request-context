8a8f2d50b643df248bd9a0b8de7976bcd3c80a09
Igor Savin <iselwin@gmail.com>
2020-08-13 15:41:08 +0200

    Add test for POST request with body (#11)

--- test/requestContextPlugin.spec.js
+++ test/requestContextPlugin.spec.js
@@ -2,11 +2,20 @@ const fastify = require('fastify')
 const { fastifyRequestContextPlugin } = require('../lib/requestContextPlugin')
 const { TestService } = require('./internal/testService')
 
-function initApp(endpoint) {
+function initAppGet(endpoint) {
   const app = fastify({ logger: true })
   app.register(fastifyRequestContextPlugin)
 
   app.get('/', endpoint)
+
+  return app.ready()
+}
+
+function initAppPost(endpoint) {
+  const app = fastify({ logger: true })
+  app.register(fastifyRequestContextPlugin)
+
+  app.post('/', endpoint)
 
   return app.ready()
 }
@@ -17,7 +26,7 @@ describe('requestContextPlugin', () => {
     return app.close()
   })
 
-  it('correctly preserves values within single request', () => {
+  it('correctly preserves values within single GET request', () => {
     expect.assertions(2)
 
     let testService
@@ -50,7 +59,7 @@ describe('requestContextPlugin', () => {
             }
           }
 
-          initApp(route).then((_app) => {
+          initAppGet(route).then((_app) => {
             app = _app
             testService = new TestService(app)
             const response1Promise = app
@@ -70,6 +79,79 @@ describe('requestContextPlugin', () => {
               .inject()
               .get('/')
               .query({ requestId: 2 })
+              .end()
+              .then((response) => {
+                expect(response.json().storedValue).toBe('testValue2')
+                responseCounter++
+                if (responseCounter === 2) {
+                  resolveResponsePromise()
+                }
+              })
+
+            return Promise.all([response1Promise, response2Promise])
+          })
+        })
+
+        return promiseRequest1
+      })
+
+      return promiseRequest2
+    })
+  })
+
+  it('correctly preserves values within single POST request', () => {
+    expect.assertions(2)
+
+    let testService
+    let responseCounter = 0
+    return new Promise((resolveResponsePromise) => {
+      const promiseRequest2 = new Promise((resolveRequest2Promise) => {
+        const promiseRequest1 = new Promise((resolveRequest1Promise) => {
+          const route = (req, reply) => {
+            function prepareReply() {
+              testService.processRequest(requestId).then(() => {
+                const storedValue = req.requestContext.get('testKey')
+                reply.status(204).send({
+                  storedValue,
+                })
+              })
+            }
+
+            const requestId = Number.parseInt(req.body.requestId)
+            req.requestContext.set('testKey', `testValue${requestId}`)
+
+            // We don't want to read values until both requests wrote their values to see if there is a racing condition
+            if (requestId === 1) {
+              resolveRequest1Promise()
+              return promiseRequest2.then(prepareReply)
+            }
+
+            if (requestId === 2) {
+              resolveRequest2Promise()
+              return promiseRequest1.then(prepareReply)
+            }
+          }
+
+          initAppPost(route).then((_app) => {
+            app = _app
+            testService = new TestService(app)
+            const response1Promise = app
+              .inject()
+              .post('/')
+              .body({ requestId: 1 })
+              .end()
+              .then((response) => {
+                expect(response.json().storedValue).toBe('testValue1')
+                responseCounter++
+                if (responseCounter === 2) {
+                  resolveResponsePromise()
+                }
+              })
+
+            const response2Promise = app
+              .inject()
+              .post('/')
+              .body({ requestId: 2 })
               .end()
               .then((response) => {
                 expect(response.json().storedValue).toBe('testValue2')
